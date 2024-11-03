34857318b3911c12f3fb44da54d4bcd31b0a88a5
Naveed Mastan <m.a.naveedmastan@gmail.com>
2024-11-03 19:26:35 -0400

    Initial commit

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,17 @@
+// src/components/ProtectedRoute.js
+
+import React from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const { authState } = useOktaAuth();
+
+  if (!authState?.isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
